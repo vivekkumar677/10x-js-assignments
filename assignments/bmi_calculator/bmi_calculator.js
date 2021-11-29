628fa014c10908ad65c9dd9c4616d6a59b198a0c
Vivek Kumar <ervivekkumar677@gmail.com>
2021-11-29 23:46:19 +0530

    favourite_movie re-submission

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -3,6 +3,8 @@ function BMICalculator(mass, height) {
     // Write your code here
     if(typeof mass==="number" && typeof height==="number" && height>0 && mass>0){
         return mass/(height*height)
+    } else{
+        return "INVALID INPUT"
     }
     
 }
